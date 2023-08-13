a26010970241f7f933be7a0707fa6190028ce30c
betology <betology@gmail.com>
2023-08-13 16:00:29 -0600

    created function to update post

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -55,3 +55,32 @@ func PostsShow(c *gin.Context) {
 	})
 
 }
+
+func PostsUpdate(c *gin.Context) {
+	// Get the id off the url
+	id := c.Param("id")
+
+	// Get the data off req body
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	// Find the post were updating
+	var post models.Post
+	initializers.DB.Find(&post, id)
+
+	// Update it
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+
+	// Respont with it
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+
+}
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/posts", controllers.PostsCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
 	r.GET("/posts", controllers.PostsIndex)
 	r.GET("/posts/:id", controllers.PostsShow)
 
